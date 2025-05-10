c200f9a6e6c90a1a94b2bd67b7051a6893573d51
Prottoy21 <prottoy.tsp.47@gmail.com>
2025-05-11 02:56:28 +0600

    adding remove button so that task can be deleted

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,25 @@
+const input = document.getElementById("input");
+const listContainer = document.getElementById("list-container");
 
-const input = document.getElementById('input');
-const listContainer = document.getElementById('list-container');
+const button = document
+  .getElementById("button")
+  .addEventListener("click", function () {
+    if (input.value === "") {
+      alert("Please add some task");
+    } else {
+      const li = document.createElement("li");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerHTML = "&times;";
+      li.innerHTML = input.value;
+      listContainer.appendChild(li);
+      deleteBtn.classList.add("delete-btn");
+      deleteBtn.onclick = function () {
+        listContainer.removeChild(li);
+      };
 
-
-const button = document.getElementById('button').addEventListener('click', function() {
-    if(input.value === "") {
-        alert("Please add some task");
+      li.appendChild(deleteBtn);
+      listContainer.appendChild(li);
     }
-    else {
-        const li = document.createElement('li');
-        li.innerText = input.value;
-        listContainer.appendChild(li); 
-    }
-    input.value = '';
-})
+    input.value = "";
+
+  });
